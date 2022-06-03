1230fd98869f26d8483b65e1b4388614fe20f377
Drew Pepin <drewpepin1995@gmail.com>
2022-06-03 19:20:11 -0400

    Importing Components to App.js

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,12 @@ import React from 'react'
 import Header from './components/Header/Header'
 import Nav from './components/Nav/Nav'
 import About from './components/About/About'
+import Experience from './components/Experience/Experience'
+import Services from './components/Services/Services'
+import Portfolio from './components/Portfolio/Portfolio'
+import Testimonials from './components/Testimonials/Testimonials'
+import Contact from './components/Contact/Contact'
+import Footer from './components/Footer/Footer'
 
 const App = () => {
   return (
@@ -9,7 +15,12 @@ const App = () => {
       <Header />
       <Nav /> 
       <About />
-
+      <Experience />
+      <Services />
+      <Portfolio />
+      <Testimonials />
+      <Contact />
+      <Footer />
     </>
   )
 }
