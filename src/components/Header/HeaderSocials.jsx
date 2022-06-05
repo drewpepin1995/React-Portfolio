05da7557331ab3efe17fbca2d6015f4d663184b1
Drew Pepin <drewpepin1995@gmail.com>
2022-06-05 19:44:20 -0400

    Styling Header buttons and Image

--- src/components/Header/HeaderSocials.jsx
+++ src/components/Header/HeaderSocials.jsx
@@ -5,7 +5,7 @@ import {BsTwitter} from 'react-icons/bs'
 
 const HeaderSocials = () => {
   return (
-    <div className='header_socials'>
+    <div className='header__socials'>
         <a href='https://linkedin.com' target="_blank"><BsLinkedin/></a>
         <a href='https://github.com' target="_blank"><BsGithub/></a>
         <a href='https://twitter.com' target="_blank"><BsTwitter/></a>
